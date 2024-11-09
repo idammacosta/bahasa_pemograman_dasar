91ab35de1d3c4922bfae6b35dc8dded3f85738a8
idammacosta <138878789+idammacosta@users.noreply.github.com>
2024-11-09 04:07:40 +0000

    Ada penjumlahan

--- index.py
+++ index.py
@@ -1,1 +1,10 @@
-print("Hello Word")
+#Program Tambah Tambahan
+print("Fungsi Penjumlahan" )
+nomor1 = input("masukan Nomor1 = ")
+nomor2 = input("Masukan Nomor2 = ")
+nomor1 = int(nomor1)
+nomor2 = int(nomor2)
+def sum(a,b):
+    print(a+b)
+
+sum (nomor1,nomor2)
