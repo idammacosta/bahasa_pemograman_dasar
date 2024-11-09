789cc0694d9c8dc3cb7be3e54402b0b0f8a9fa0a
idammacosta <138878789+idammacosta@users.noreply.github.com>
2024-11-09 04:14:43 +0000

    aman

--- index.py
+++ index.py
@@ -2,9 +2,10 @@
 print("Fungsi Penjumlahan" )
 nomor1 = input("masukan Nomor1 = ")
 nomor2 = input("Masukan Nomor2 = ")
-nomor1 = int(nomor1)
-nomor2 = int(nomor2)
+nomor1 = float(nomor1)
+nomor2 = float(nomor2)
 def sum(a,b):
     print(a+b)
 
 sum (nomor1,nomor2)
+print(type(sum))
