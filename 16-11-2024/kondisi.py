32e0e437edde68647d7b592cfc3339e4c1f14370
idammacosta <138878789+idammacosta@users.noreply.github.com>
2024-11-16 03:59:07 +0000

    aman bgt

---
+++ 16-11-2024/kondisi.py
@@ -0,0 +1,6 @@
+nilai_akhir_siswa = int(input("Masukkan Nilai:"))
+if nilai_akhir_siswa >= 60 :
+    keterangan = "lulus"
+else:
+    keterangan = "Gagal"
+    print("Anda:",keterangan,"Dengan Nilai :",nilai_akhir_siswa)
