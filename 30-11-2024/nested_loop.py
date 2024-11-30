86506ab59ea7ee1a85b421f025b28470d4cf1777
idammacosta <138878789+idammacosta@users.noreply.github.com>
2024-11-30 04:19:02 +0000

    Nambah buat perualnagn gambar segitiga

--- 30-11-2024/nested_loop.py
+++ 30-11-2024/nested_loop.py
@@ -2,3 +2,9 @@ for i in range(1,5,1):
     for j in range(0,3,1):
         print(i*j, end="")
     print ()
+
+baris = int(input('tentukan baris dari segitiga: '))
+for i in range(1,baris+1):
+    for j in range(1,i+1):
+        print('*', end=" ")
+    print()
