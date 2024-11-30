20880df07ecfacfae4a265bcdcfe2ee246f2c2d5
idammacosta <138878789+idammacosta@users.noreply.github.com>
2024-11-30 04:25:16 +0000

    Ngedit dikit biara rapi

--- 30-11-2024/nested_loop.py
+++ 30-11-2024/nested_loop.py
@@ -3,6 +3,7 @@ for i in range(1,5,1):
         print(i*j, end="")
     print ()
 
+print("\n")
 baris = int(input('tentukan baris dari segitiga: '))
 for i in range(1,baris+1):
     for j in range(1,i+1):
