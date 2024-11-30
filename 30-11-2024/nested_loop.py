8db710f7629cdec041f15ab3fec7ae87e2fa2b90
idammacosta <138878789+idammacosta@users.noreply.github.com>
2024-11-30 04:37:17 +0000

    nambah segitiga pokoknya

--- 30-11-2024/nested_loop.py
+++ 30-11-2024/nested_loop.py
@@ -3,9 +3,20 @@ for i in range(1,5,1):
         print(i*j, end="")
     print ()
 
+
+print("\n")
+baris = int(input('tentukan baris dari segitiga: '))
+angka = 1
+for i in range(1,baris+1):
+    for j in range(1,i+1):
+        print(angka, end=" ")
+        angka += 1
+    print()
+
 print("\n")
 baris = int(input('tentukan baris dari segitiga: '))
 for i in range(1,baris+1):
     for j in range(1,i+1):
-        print('*', end=" ")
+        print(i, end=" ")
+        i += 1
     print()
