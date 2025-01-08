e5a2d5a318a33edf6c7e4be94bc747d9f2ffdf5a
idammachosta <138878789+idammacosta@users.noreply.github.com>
2025-01-08 08:01:53 +0000

    c

--- 08-01-2025/UAS.py
+++
@@ -1,8 +0,0 @@
-# Tanpa variabel
-print(3.14 * 10 * 10)  # Tidak jelas apa yang dihitung
-
-# Dengan variabel
-jari_jari = 10
-pi = 3.14
-luas_lingkaran = pi * jari_jari * jari_jari
-print(luas_lingkaran)  # Lebih jelas bahwa ini menghitung luas lingkaran
