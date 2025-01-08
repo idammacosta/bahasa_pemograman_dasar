ed3457f58783e186be580c5abbe385e493d763f7
idammachosta <138878789+idammacosta@users.noreply.github.com>
2025-01-08 08:18:56 +0000

    update

---
+++ 08-01-2025/uas_idam_no2.py
@@ -0,0 +1,21 @@
+nilaiTK = [90, 75, 95, 85, 80]
+
+print("=" * 60)
+print(" " * 24 + "PROGRAM LIST")
+print(" " * 15 + f"nilaiTK = {nilaiTK}")
+print("=" * 60)
+print("Mencetak semua isi list:")
+print(nilaiTK)
+print("=" * 60)
+
+print("Mencetak isi list index ke-1")
+print(nilaiTK[1])
+print("=" * 60)
+
+print("Mencetak isi list mulai dari index ke-1 sampai index ke-2")
+print(nilaiTK[1:3])
+print("=" * 60)
+
+nilaiKu = nilaiTK[2]
+print("Mengambil isi list index ke-2 ke variabel nilaiKu")
+print(f"nilaiKu = {nilaiKu}")
